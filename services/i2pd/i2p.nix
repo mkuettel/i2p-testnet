fb5ff1cea7650a3985845b76d087a6f8a7d744db
Moritz Küttel <moritz.kuettel@gmail.com>
2021-03-24 14:18:44 +0100

    add more options as comments for i2pd service

--- services/i2pd/i2p.nix
+++ services/i2pd/i2p.nix
@@ -22,6 +22,8 @@ in {
       # subscriptions
     # };
     bandwidth = 100; # limit bandwidth in KBbs (if not set it's 32 KBps)
+    share = 100; # amount of traffic to be transit traffic in %
+
     # dataDir = /var/i2pdata;
     exploratory = {
       inbound = {
@@ -34,6 +36,12 @@ in {
       };
     };
     family = "mognet";
+
+    trust = {
+      # family = # router familiy to trust for first hop
+      # hidden = false; # enable router concealment
+      routers = [ ]; # only connect to listed routers
+    };
 
     # uncomment if router is unrachable and needs introduction nodes
     # floodfill = true;
@@ -58,5 +66,24 @@ in {
     #limits = {
     #  coreSize = ;
     #};
+  };
+
+  reseed = {
+    # file = ;
+    # floodfill
+    # urls
+    # verify
+  };
+
+  # upnp = {
+  #   enable = false;
+  #   name = "I2Pd";
+  # };
+
+  websocket = {
+    # enable = true;
+    address = "127.0.0.1";
+    name = "websockets";
+    port = 7666; #default
   };
 }
