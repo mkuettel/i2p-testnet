77eb9c909870719523904abfe4ebc221d77b7da1
Moritz Küttel <moritz.kuettel@gmail.com>
2021-03-25 15:57:48 +0100

    fix overlay to use original i2pd package name
    because the i2pd module doesn't allow setting a package
    we need to override the nixpkgs i2pd package.

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -3,9 +3,9 @@
 {
   nixpkgs.overlays = [
     (self: super: rec {
-      mogi2pd = super.callPackage ./pkgs/i2pd { };
+      i2pd = super.callPackage ./pkgs/i2pd { };
       i2p-tools = super.callPackage ./pkgs/i2p-tools {
-        inherit mogi2pd;
+        inherit i2pd;
       };
     })
   ];
