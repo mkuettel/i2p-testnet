0cc498bb29b93330b4b8e633cabe710cd5c88be6
Moritz Küttel <moritz.kuettel@gmail.com>
2021-03-26 11:35:20 +0100

    use set nix path to channel 20.09 on deployment host and targets
    This makes it more reproducible and we get more stable packages.
    And it also makes the guest extensions play nice with my VirtualBox
    version on the host system.

--- mypkgs.nix
+++ mypkgs.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }: 
 
 {
+  # For now just use the stable channel for less breakage
+  # TODO: pin this?
+  nix.nixPath = [ "nixpkgs=https://nixos.org/channels/nixos-20.09/nixexprs.tar.xz" ];
+
   nixpkgs.overlays = [
     (self: super: rec {
       i2pd = super.callPackage ./pkgs/i2pd { };
