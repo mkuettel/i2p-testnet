a70e26f939c0fb319d50d586e22b36b4c09aab4d
Moritz Küttel <moritz.kuettel@gmail.com>
2021-03-26 15:15:48 +0100

    correctly configure i2p reseed service so it launches
    This requires some certificates for the service to be able to sign the
    su3 files.

--- mypkgs.nix
+++ mypkgs.nix
@@ -9,6 +9,17 @@
     (self: super: rec {
       i2pd = super.callPackage ./pkgs/i2pd { };
       i2p-tools = super.callPackage ./pkgs/i2p-tools { };
+      i2p-testnet-reseed-keys = super.stdenv.mkDerivation rec {
+        pname = "i2p-testnet-reseed-keys";
+        version = "0.0.1";
+        src = ./keys/su3;
+        installPhase = ''
+          mkdir $out
+          cp $src/mkuettel_at_mail.* $out
+        '';
+
+
+      };
     })
   ];
 }
