f84020383884c7bbd63d0eef698ff63f7c34804a
Moritz Küttel <moritz.kuettel@gmail.com>
2021-05-06 15:27:04 +0200

    fix startup of tcpserver in container

--- docker/i2p/entrypoint-tcpsrv.sh
+++ docker/i2p/entrypoint-tcpsrv.sh
@@ -1,5 +1,8 @@
+#!/bin/sh
+
 # start the message logging tcp server and log to the volume shared with the host
-/usr/local/bin/tcpserver /home/i2pd/data/msglog.csv
+su i2pd -c 'mkdir /home/i2pd/data/messagelogs'
+su i2pd -c '/usr/local/bin/tcpserver /home/i2pd/data/messagelogs/msglog.csv' 2323 &
 
 # now that the TCP server is running use the normal entry point which starts i2pd
-/entrypoint.sh
+exec /entrypoint.sh
