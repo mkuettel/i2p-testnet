8639e9376565d06673cf5deea886906bbedf3990
Moritz Küttel <moritz.kuettel@gmail.com>
2021-05-07 15:09:42 +0200

    log the tcpserver output

--- docker/i2p/entrypoint-tcpsrv.sh
+++ docker/i2p/entrypoint-tcpsrv.sh
@@ -2,7 +2,8 @@
 
 # start the message logging tcp server and log to the volume shared with the host
 su i2pd -c 'mkdir /home/i2pd/data/messagelogs'
-su i2pd -c '/usr/local/bin/tcpserver /home/i2pd/data/messagelogs/msglog.csv' 2323 &
+su i2pd -c 'touch /home/i2pd/data/messagelogs/tcpserver.log'
+su i2pd -c '/usr/local/bin/tcpserver /home/i2pd/data/messagelogs/msglog.csv' > /home/i2pd/data/messagelogs/tcpserver.log 2>&1  &
 
 # now that the TCP server is running use the normal entry point which starts i2pd
 exec /entrypoint.sh
